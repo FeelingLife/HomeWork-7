61be873173b0a5a33b11ddc74e75585f73d2a3c9
Nikita <crazefire@mail.ru>
2023-05-18 19:22:50 +0300

    FUD

---
+++ DZ_47.cs
@@ -0,0 +1,33 @@
+﻿//Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами от -10,0 до 10,0.
+
+int MethodFillingArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[m, n] = new Random().Next(-10, 11);
+            Console.Write(array[m, n] + " ");
+        }
+        return n;
+    }
+    return m;
+}
+MethodFillingArray(2, 5);
+
+// void MethodPrintArray(string message)
+// {
+//     int[,] array = new int[m, n];
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write($"{array[m, n]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+
+// MethodPrintArray("Так выглядит массив: ");

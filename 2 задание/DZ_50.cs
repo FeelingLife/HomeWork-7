c2e4a9e9d8ee27ff4080093afc8ac8328de76e37
Nikita <crazefire@mail.ru>
2023-05-19 21:59:35 +0300

    2 dz complete

--- 2 задание/DZ_50.cs
+++ 2 задание/DZ_50.cs
@@ -1,53 +1,52 @@
 ﻿// Задача 50. Напишите программу, которая на вход принимает число, и возвращает индексы числа в двумерном массиве или же указание, что такого элемента нет.
 
-int MethodPrintNumbers(int finder, int rows, int columns)
-{
-    Console.WriteLine();
-    {
-        Console.WriteLine($"Bесомое число: {finder}");
-        return finder;
-    }
-    {
-        Console.WriteLine($"Количество строк массива: {rows}");
-        return rows;
-    }
-    {
-        Console.WriteLine($"Количество столбцов массива: {columns}");
-        return columns;
-    }
-    Console.WriteLine();
-}
+// Код при помощи методов методов
 
-int[,] array = new int[rows, columns];
-int q = 0;
-
-for (int i = 0; i < rows; i++)
+int[,] CreatArray(int rows, int columns) //Создаем массив с выполненной задачей
 {
-    for (int j = 0; j < columns; j++)
+    Console.WriteLine("Количество строк массива: " + rows + " " );
+    Console.WriteLine("Количество столбцов массива: " + columns + " " );
+    Console.Write("Введите искомое число: ");
+    int finder = int.Parse(Console.ReadLine());
+    int[,] array = new int[rows, columns];
+    int q = 0; // счетчик
+    for (int i = 0; i < rows; i++)
     {
-        array[i, j] = new Random().Next(0, 21);
-        if (finder == array[i, j])
+        for (int j = 0; j < columns; j++)
         {
-            Console.WriteLine($"Индексы числа в двумерном массиве: ({i + 1},{j + 1}) ");
-            q++;
+            array[i, j] = new Random().Next(0, 21);
+            if (finder == array[i, j])
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Индексы числа в двумерном массиве: ({i + 1},{j + 1}) ");
+                q++;
+            }
         }
     }
+    if (q == 0)
+    {
+        Console.WriteLine($"{finder} - такого числа в масссиве нет");
+    }
+    return array;
 }
-if (q == 0)
+void PrintArray(int[,] array) //создаем вывод массива на экран
 {
-    Console.WriteLine($"{finder} - такого числа в масссиве нет");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+
 }
+int[,] massive = CreatArray(5, 5);
+Console.WriteLine();
+PrintArray(massive);
 Console.WriteLine();
 
-for (int i = 0; i < array.GetLength(0); i++)
-{
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-        Console.Write($"{array[i, j]} ");
-    }
-    Console.WriteLine();
-}
-Console.WriteLine();
+// Код без методов
 
 // Console.WriteLine();
 // Console.Write("Введите искомое число: ");
